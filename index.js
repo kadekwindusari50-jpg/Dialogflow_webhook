78c5e83d7564ca494df2e4360aa322591830e582
kadekwindusari50-jpg <kadekwindusari50@gmail.com>
2026-02-12 11:27:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,38 @@ app.post("/webhook", (req, res) => {
     const hari = req.body.queryResult.parameters.hari;
 
     const jadwal = {
-        senin: "08:00 - Matematika, 10:00 - Bahasa Indonesia",
-        selasa: "08:00 - IPA, 10:00 - IPS",
-        rabu: "08:00 - Bahasa Inggris, 10:00 - Seni Budaya",
-        kamis: "08:00 - PJOK, 10:00 - PPKn",
-        jumat: "08:00 - Agama, 09:30 - Prakarya"
+        senin: `
+08:10 - 10:10 Agama
+10:40 - 15:00 Dasar dasar akuntansi`,
+
+        selasa: `
+07:30 - 09:30 Pjok
+09:30 - 11:20 Matematika
+11:20 - 15:40 Ipas`,
+
+        rabu: `
+07:30 - 08:50 Sejarah
+08:50 - 13:20 Dasar dasar akuntansi
+13:40 - 15:00 Kka`,
+
+        kamis: `
+07:30 - 08:50 Bahasa indonesia
+08:50 - 12:00 Informatika
+12:00 - 15:00 Bahasa inggris
+15:00 - 15:40 Bk`,
+
+        jumat: `
+08:10 - 09:30 Seni budaya
+09:30 - 11:20 Ppkn
+11:20 - 12:40 Bahasa indonesia
+12:40 - 14:20 Bahasa bali
+14:20 - 15:40 Matematika
     };
 
-    let jawaban = jadwal[hari.toLowerCase()] || "Jadwal tidak ditemukan";
+    let jawaban = jadwal[hari?.toLowerCase()] || "Jadwal tidak ditemukan";
 
     res.json({
-        fulfillmentText: `Jadwal hari ${hari} adalah ${jawaban}`
+        fulfillmentText: `Jadwal hari ${hari} adalah:\n${jawaban}`
     });
 });
 
